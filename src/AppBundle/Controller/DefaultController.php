8264c4402221e46b9a22868c47107864437c23f3
Tristan Perchec <tristan.perchec@yproximite.com>
2016-11-15 14:59:25 +0100

    added some routes and form for trip

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Trip;
+use AppBundle\Form\TripType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +17,41 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/trip/{id}", name="trip")
+     */
+    public function tripAction(Request $request, Trip $trip)
+    {
+        return $this->render(
+            'trip/show.html.twig',
+            ['trip' => $trip]
+        );
+    }
+
+    /**
+     * @Route("create/trip", name="create_trip")
+     */
+    public function createTripAction(Request $request)
+    {
+        $trip = new Trip();
+        $form = $this->createForm(TripType::class, $trip);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($trip);
+            $em->flush();
+
+            return $this->redirectToRoute('trip', ['id' => $trip->getId()]);
+        }
+
+        return $this->render(
+            'trip/form.html.twig',
+            ['form' => $form->createView()]
+        );
     }
 }
